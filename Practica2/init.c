8aa833e3c16116e9cf0026110e8aa59426e37026
David Perez Daher <david.pd2690@gmail.com>
2018-09-01 10:25:27 -0500

    init succesfully creates 6 proceses

---
+++ Practica2/init.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <signal.h>
+
+#define NPROCS 6
+
+int main(int argc, char const *argv[])
+{
+    pid_t pidt;
+    
+    int i;
+    int pid, pidArr, sd;
+   // int status;
+    int endProcess;
+
+    int *shutdown = 0;
+    int *sdptr;
+    int *pids;
+    int *pidsptr;
+
+    sdptr = (int*) malloc(sd * sizeof(int));
+    pidsptr = (int*) malloc(pidArr * sizeof(int));
+
+    shutdown = sdptr;
+    pids = pidsptr;
+
+    for(i=0;i<NPROCS;i++)
+    {
+        pid = fork();
+
+        if(pid == 0)
+        {
+            execl("/usr/bin/xterm", "/usr/bin/xterm", "-e", "./getty.exe", NULL);
+        }
+
+        else pids[i] = pid;
+
+    }
+
+    while(!*shutdown)
+    {
+        for(i = 0; i < NPROCS; i++)
+        {
+            endProcess = kill(pids[i], 0);
+            if(endProcess < 0)
+            {
+                pid = fork();
+            }
+
+            if(pid == 0)
+            {
+                execl("/usr/bin/xterm", "/usr/bin/xterm", "-e", "./getty.exe", NULL);
+            }
+        }
+    }
+    return 0;
+}
+
+
